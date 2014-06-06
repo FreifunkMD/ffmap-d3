3f2b608dd32b0ce49f94b3271bafba12a7a83274
root <root@v22014052223718720.yourvserver.net>
2014-06-06 16:25:34 +0200

    config for magdeburg

--- config.js
+++ config.js
@@ -2,10 +2,10 @@
 
 var ffmapConfig = {
   // link to your main community site:
-  url:       "https://hamburg.freifunk.net",
+  url:       "http://md.freifunk.net",
   
   // visible link in the navigation:
-  sitename:  "hamburg.freifunk.net",
+  sitename:  "md.freifunk.net",
   
   // initial gravity, friction, of the graph at pageload:
   gravity:   0.05,
@@ -14,6 +14,8 @@ var ffmapConfig = {
   charge:    1.0,
   distance:  1.0,
   strength:  1.0,
+  
+  current_firmware : "0.26"
 
   // path to the nodes.json
   nodes_json: "nodes.json",
